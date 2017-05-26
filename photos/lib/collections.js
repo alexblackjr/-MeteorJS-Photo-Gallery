464a738856cb0652c36e5a9a71edd31eeb2cb40c
Alexander Black <alexblackjr@gmail.com>
2017-05-25 22:38:41 -0400

    finished code

---
+++ photos/lib/collections.js
@@ -0,0 +1,19 @@
+/**
+ * Created by Alexander on 5/25/2017.
+ */
+Photos = new FS.Collection( 'Photos', {
+   stores: [ new FS.Store.GridFS( 'Photos') ],
+        filter: {
+            allow: {
+                contentTypes: [ 'image/*' ]
+            },
+            onInvalid: function ( message ) {
+                FlashMessage.sendError( message );
+            }
+        }
+});
+
+Photos.allow({
+    insert: function(){ return true; },
+    download: function(){ return true; }
+});
